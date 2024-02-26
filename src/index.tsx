a7183524fba2d09338f2e88f351d056768330903
maru0pengin <iuchi-masatoshi@dmm.com>
2024-02-26 11:24:16 +0900

    hello world表示

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,6 @@ interface AppProps {
 const App: React.FC<AppProps> = ({ message }) => <div>{message}</div>;
 
 ReactDOM.render(
-  <App message="Hello, World with TypeScript and React!" />,
+  <App message="Hello, World with TypeScript and React! hogehoge" />,
   document.getElementById("root")
 );
