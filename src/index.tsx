9b996f5a821131cc0b86c73568f366ff2b29797f
maru0pengin <iuchi-masatoshi@dmm.com>
2024-02-26 10:49:08 +0900

    tsのbuildを確認

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+// src/index.tsx
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+interface AppProps {
+  message: string;
+}
+
+const App: React.FC<AppProps> = ({ message }) => <div>{message}</div>;
+
+ReactDOM.render(
+  <App message="Hello, World with TypeScript and React!" />,
+  document.getElementById("root")
+);
